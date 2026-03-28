40072365554fa762753d5238555bff28631d6078
N7N0C <N7N0C@IMAMABUUMAR>
2026-03-28 21:59:33 +0700

    Add project files.

---
+++ HelloWord/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace HelloWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Saya lagi belajar C#");
+            Console.WriteLine("Kayakanya seru juga");
+            Console.WriteLine(6+7);
+            Console.Write("ini menggunakan Write saja tanpa WriteLine");
+            Console.Write("coba ini baris ke dua tanpa WirteLine");
+            string nama = "Imam";
+            Console.WriteLine("\nNama saya " + nama);
+            int umur = 38;
+            Console.WriteLine("\nUmur saya " + umur);
+            const double phi = 3.14;
+            Console.WriteLine("\nNilai phi adalah " + phi);
+           // phi = 3.14159; // Ini akan menghasilkan error karena phi adalah konstanta
+        }
+    }
+}
